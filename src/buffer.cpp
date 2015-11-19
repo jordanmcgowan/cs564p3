bac2ad4d433935e2a2dee3946d26ca2256844b71
jordanmcgowan <jordan_mcgowan@yahoo.com>
2015-11-19 11:08:27 -0600

    Update and cleanup

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -37,22 +37,23 @@ BufMgr::BufMgr(std::uint32_t bufs)
 //Flushes out all dirty pages and deallocates the buffer pool and the BufDesc table.
 BufMgr::~BufMgr() 
 {
+  //Creates a pointer to each buffer in the descTable
   for (int i = 0; i < numBufs; i++){
     BufDesc *tmpbuf = &bufDescTable[i];
+    //Checks for modified (but not 'saved') buffers and then writes them to mem
     if (tmpbuf->valid == true && tmpbuf->dirty == true){
-      cout << "flushing page " <<  tmpbuf->pageNo << "from frame " << i << endl;
       tmpbuf->file->writePage(tmpbuf->pageNo, &(bufPool[i]));
     }
   }
+  //Removes the nessecary additional vars
   delete [] bufDescTable;
   delete [] bufPool;
-  delete hashTable;
 }
 
 //Advance clock to next frame in the buffer pool.
 void BufMgr::advanceClock()
 {
-
+  //HALP
 }
 
 //
@@ -65,58 +66,61 @@ void BufMgr::allocBuf(FrameId & frame)
     advanceClock();
     numScanned++;
 
-    if (! bufDescTable[clockHand]->valid) {
+    //Checks for a valid segment of the clock 
+    if (!bufDescTable[clockHand]->valid) {
       break;
     }
 
-    if (! bufDescTable[clockHand]->refbit) {
+    //Checks to ensure this clock segment has not been used recently
+    if (!bufDescTable[clockHand]->refbit) {
+      //Checks to ensure this clock segment has not been pinned
       if (bufDescTable[clockHand]->pinCnt == 0) {
+        //Remove the entry from the hash table and ACK that a position has been found
         hashTable->remove(bufDescTable[clockHand]->file, bufDescTable[clockHand]->pageNo);
         found = true;
         break;
       }
     }
+    //Skips the clock segment and updates vars
     else {
       bufStats->accesses++;
       bufDescTable[clockHand]->refbit = false;
     }
   }
-
-  if (! found && numScanned >= 2*numBufs){
+  //Throws execption if a buffer cannot be allocated
+  if (!found && numScanned >= 2*numBufs){
     throw buffer_exceeded_exception();
   }
-
+  //Write to the disk if the clock segment hasnt been written since an update (segment is dirty)
   if (bufDescTable[clockHand]->dirty){
     bufStats->diskwrites++;
     bufDescTable[clockHand]->file->writePage(bufDescTable[clockHand]->pageNo, bufPool[clockHand]);
   }
 
   bufDescTable[clockHand]->Clear();
-
   frame = clockHand;
-
 }
-
 	
 void BufMgr::readPage(File* file, const PageId pageNo, Page*& page)
 {
   FrameId frameNo = 0;
+  //Attempts to load page from mem based on params 
   try {
     hashTable->lookup(file, pageNo, frameNo);
     bufDescTable[frameNo]->refbit = true;
     bufDescTable[frameNo]->pinCnt++;
     page = &bufPool[frameNo];
   }
+  //
   catch (hash_not_found_exception e){
     allocBuf(frameNo);
-
     bufStats->diskreads++;
     bufPool[frameNo] = file->readPage(pageNo);
-
+    hashTable->insert(file, pageNo, frameNo);
     bufDescTable[frameNo]->Set(file, pageNo);
+    //Return a pointer to the frame containing the page via the page parameter
     page = &bufPool[frameNo];
 
-    hashTable->insert(file, pageNo, frameNo);
   }
 }
 
@@ -126,20 +130,20 @@ void BufMgr::unPinPage(File* file, const PageId pageNo, const bool dirty)
   hashTable->lookup(file, pageNo, frameNo);
 
   if (dirty == true){
-    bufDescTable[frameNo].dirty = dirty;
+    bufDescTable[frameNo].dirty = true;
   }
 
   if (bufDescTable[frameNo]->pinCnt == 0){
     throw page_not_pinned_exception(file->filename(), pageNo, frameNo);
   }
-  else 
-    bufDescTable[frameNo]->pinCnt--;
+  bufDescTable[frameNo]->pinCnt--;
 }
 
 void BufMgr::flushFile(const File* file) 
 {
   for (int i = 0; i < numBufs; i++){
     BufDesc *tmpbuf = &(bufDescTable[i])
+    //'Scan[s] bufTable for pages belonging to the file'
     if (tmpbuf->valid == true && tmpbuf->file == file){
       if (tmpbuf->pinCnt = 0){
         throw page_pinned_exception(file->filename(), tmpbuf->pageNo, tmpbuf->frameNo);
@@ -160,14 +164,14 @@ void BufMgr::flushFile(const File* file)
 void BufMgr::allocPage(File* file, PageId &pageNo, Page*& page) 
 {
   FrameId frameNo;
-  allocBuf(frameNo);
-
   bufPool[frameNo] = file->allocPage(pageNo);
+  allocBuf(frameNo);
+  hashTable->insert(file, pageNo, frameNo);
+  bufDescTable[frameNo].Set(file, pageNo);
   page = &bufPool[frameNo];
 
-  bufDescTable[frameNo].Set(file, pageNo);
+  return(page, frameNo);
 
-  hashTable->insert(file, pageNo, frameNo)
 }
 
 void BufMgr::disposePage(File* file, const PageId PageNo)
